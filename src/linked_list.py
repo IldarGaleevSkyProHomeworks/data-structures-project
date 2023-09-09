80bf8ce227da58dce66bf26f6343b5e4118c497a
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-09-09 21:46:19 +0700

    homework_5

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,57 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data: dict, next_node=None):
+        self._data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self._data
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self._first_element = None
+        self._last_element = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        new_node = Node(data, self._first_element)
+        self._first_element = new_node
+
+        if self._last_element is None:
+            self._last_element = new_node
+
+    @property
+    def head(self) -> Node:
+        return self._first_element
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+
+        # == for head-only way: ==
+        #
+        # curr_node = self._first_element
+        # while curr_node:
+        #     next_node = curr_node.next_node
+        #     if next_node is None:
+        #         next_node = Node(data)
+        #         curr_node.next_node = next_node
+        #         return
+        #
+        # self.insert_beginning(data)
+        # ========================
+
+        if self._last_element is None:
+            self.insert_beginning(data)
+            new_node = self._last_element
+        else:
+            new_node = Node(data)
+        self._last_element.next_node = new_node
+        self._last_element = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +61,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,26 @@
-"""Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import pytest
+
+from src.linked_list import LinkedList
+
+
+@pytest.fixture
+def linked_list_fixture():
+    return LinkedList()
+
+
+def test_linked_list_insert_beginning(linked_list_fixture):
+    linked_list_fixture.insert_beginning({'id': 0})
+    linked_list_fixture.insert_beginning({'id': 1})
+
+    assert str(linked_list_fixture) == "{'id': 1} -> {'id': 0} -> None"
+
+
+def test_linked_list_insert_at_end(linked_list_fixture):
+    linked_list_fixture.insert_at_end({'id': 0})
+    linked_list_fixture.insert_at_end({'id': 1})
+
+    assert str(linked_list_fixture) == "{'id': 0} -> {'id': 1} -> None"
+
+
+def test_linked_list_empty_str(linked_list_fixture):
+    assert str(linked_list_fixture) == 'None'
